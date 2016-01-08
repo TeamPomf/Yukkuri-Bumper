f95ebfbfd409d6779c0dbe2497bf3d2e9960d96d
BryceX <btau20102@yahoo.com>
2016-01-07 16:43:09 -0800

    First Push
    Controllable player sphere
    food deletes on player collision

---
+++ Assets/Scripts/CollectFood.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectFood : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision food)
+    {
+
+        if (food.gameObject.tag == "Food")
+        {
+
+            Destroy(food.gameObject);
+        }
+
+    }
+
+    void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+     
+    }
+    void Grow()
+    {
+       // transform.localScale.x 
+    }
+    
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    Rigidbody myRigidBody;
+
+// Use this for initialization
+    void Start ()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+    void Move()
+    {
+        if (Input.GetKey("up"))
+        {
+            myRigidBody.AddForce(Vector3.forward * Time.deltaTime*100);
+        };
+        if (Input.GetKey("down"))
+        {
+            myRigidBody.AddForce(Vector3.back * Time.deltaTime*100);
+        };
+        if (Input.GetKey("left"))
+        {
+            myRigidBody.AddForce(Vector3.left * Time.deltaTime * 100);
+        };
+        if (Input.GetKey("right"))
+        {
+            myRigidBody.AddForce(Vector3.right * Time.deltaTime * 100);
+        };
+    }
+}
