7906a1bd60375442a5fd550b5bc3e29b4323dcd9
BryceX <btau20102@yahoo.com>
2016-01-08 17:44:29 -0800

    started

---
+++ Assets/Scripts/Bump.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bump : MonoBehaviour {
+
+    public Rigidbody opponent;
+    void OnCollisionEnter(Collision opponent)
+    {
+
+        if (opponent.gameObject.tag == "Player")
+        {
+          
+        }
+        
+    }
+    void Start ()
+    {
+        opponent.
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+}
--- Assets/Scripts/MovementP2.cs
+++ Assets/Scripts/MovementP2.cs
@@ -1,35 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movement : MonoBehaviour {
-
+public class MovementP2 : MonoBehaviour {
     Rigidbody rb;
-
-// Use this for initialization
-    void Start ()
+	// Use this for initialization
+	void Start ()
     {
         rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         Move();
 	}
     void Move()
     {
-        if (Input.GetKey("w"))
+        if (Input.GetKey("up"))
         {
-            rb.AddForce(Vector3.forward * Time.deltaTime*200);
+            rb.AddForce(Vector3.forward * Time.deltaTime * 200);
         };
-        if (Input.GetKey("s"))
+        if (Input.GetKey("down"))
         {
-            rb.AddForce(Vector3.back * Time.deltaTime* 200);
+            rb.AddForce(Vector3.back * Time.deltaTime * 200);
         };
-        if (Input.GetKey("a"))
+        if (Input.GetKey("left"))
         {
             rb.AddForce(Vector3.left * Time.deltaTime * 200);
         };
-        if (Input.GetKey("d"))
+        if (Input.GetKey("right"))
         {
             rb.AddForce(Vector3.right * Time.deltaTime * 200);
         };
