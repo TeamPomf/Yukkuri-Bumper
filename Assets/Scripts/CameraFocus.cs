f396bbca5779dc1d82b7ebb09b3998d72297c70d
tyler <tyler.miller@students.aie.edu.au>
2016-01-14 17:11:33 -0800

    ANOTH?ERPUSH

--- Assets/Scripts/CameraFocus.cs
+++ Assets/Scripts/CameraFocus.cs
@@ -7,22 +7,25 @@ public class CameraFocus : MonoBehaviour
     public GameObject obj1;
     public GameObject obj2;
     public Camera cam;
-    public GameObject WinSquare;
-    Vector3 cameraOffset, obj1Offset, obj2Offset;
-    int playerWin;
+    public GameObject WinSquare, otherWinSquare;
+    Vector3 cameraOffset;//, obj1Offset, obj2Offset;
+    public int playerWin;
 
 	// Use this for initialization
 	void Start ()
     {
         playerWin = 0;
         cameraOffset = cam.transform.position;
-        obj1Offset = obj1.transform.position;
-        obj2Offset = obj2.transform.position;
+        //obj1Offset = obj1.transform.position;
+        //obj2Offset = obj2.transform.position;
 	}
 
     void OnTriggerEnter(Collider cd)
     {
+        if (cd.gameObject.name == obj1.name || cd.gameObject.name == obj2.name)
+        {
 
+        }
     }
     
     
@@ -37,21 +40,24 @@ public class CameraFocus : MonoBehaviour
         else //playerWin == 2 or 1
         {
             //WinSquare.
-            obj1.transform.position = obj1Offset;
-            obj2.transform.position = obj2Offset;
+            //obj1.transform.position = obj1Offset;
+            //obj2.transform.position = obj2Offset;
             cam.transform.position = cameraOffset;
         }
 
-        if (transform.position.y < 0)
+        if (transform.position.y < -1)
         {
             if (obj1.transform.position.y < obj2.transform.position.y)
             {
                 playerWin = 2;
+                otherWinSquare.GetComponent<Renderer>().enabled = true;
                 //PLAYER TWO WINS
             }
             else
             {
                 playerWin = 1;
+                WinSquare.GetComponent<Renderer>().enabled = true;
+
                 //PLAYER ONE WINS
             }
         }
