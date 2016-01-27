46ed52f40aa182d90fe0b47a4038f558e43bbd92
BryceX <btau20102@yahoo.com>
2016-01-27 15:49:33 -0800

    minor

--- Assets/Scripts/SpawnFood.cs
+++ Assets/Scripts/SpawnFood.cs
@@ -8,19 +8,19 @@ public class SpawnFood : MonoBehaviour {
     GameObject food1;
     [SerializeField]
     GameObject food2;
-    public float startDelay = 0;
     public int foodToSpawn = 40;
-    public float minTime = 4;
+    public int food0Delay = 5;
+    public int food1Delay = 9;
+    public int food2Delay = 15;
+    public float minTime = 8;
     public float maxTime = 10;
-
-    public GameObject[] foodStuff;
     
 
 	void Start ()
     {
-        StartCoroutine(Spawn(food0,foodToSpawn,minTime, maxTime));
-        StartCoroutine(Spawn(food1, foodToSpawn,minTime, maxTime + 20));
-        StartCoroutine(Spawn(food2, foodToSpawn,minTime, maxTime + 20));
+        StartCoroutine(Spawn(food0,foodToSpawn,minTime, maxTime, food0Delay));
+        StartCoroutine(Spawn(food1, foodToSpawn,minTime, maxTime + 30, food1Delay));
+        StartCoroutine(Spawn(food2, foodToSpawn,minTime, maxTime + 40, food2Delay));
     }
 
 
@@ -31,10 +31,10 @@ public class SpawnFood : MonoBehaviour {
 	}
     
 
-    IEnumerator Spawn(GameObject prefab, int amount, float timeMin, float timeMax)
+    IEnumerator Spawn(GameObject prefab, int amount, float timeMin, float timeMax, float delay)
     {
 
-        yield return new WaitForSeconds(startDelay);
+        yield return new WaitForSeconds(delay);
 
         for (int i = 0; i < amount; i++)
         {
