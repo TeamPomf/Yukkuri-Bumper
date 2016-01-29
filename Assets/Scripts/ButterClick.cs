c9ae84e2c1352f1bd8ef4fac5a314fc32dafee2b
tyler <tyler.miller@students.aie.edu.au>
2016-01-29 11:38:00 -0800

    MENU ADDED

---
+++ Assets/Scripts/ButterClick.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButterClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+    public void ArenaClick()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
